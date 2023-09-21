bf3f9851edc9cbd2d4d5a5cd31ee65d69521d63c
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-22 00:58:58 +0800

    add, modify, and delete

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDmLIv1C19LwO43TYymk8lTRZZYHmZzSqQ",
+    authDomain: "clone-664be.firebaseapp.com",
+    projectId: "clone-664be",
+    storageBucket: "clone-664be.appspot.com",
+    messagingSenderId: "58208125075",
+    appId: "1:58208125075:web:6616ad63007e417f27cd66",
+    measurementId: "G-6ZQQC4NLK9"
+  };
