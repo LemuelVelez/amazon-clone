f3237b9d1f0658e51f5592c372f46bf01888d949
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-23 17:45:02 +0800

    import header to app.js

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Header.css";
+// import "./Header.css";
 
 function Header() {
   return (
@@ -18,6 +18,16 @@ function Header() {
       </div>
 
       <div className="header__nav">
+
+        <div className='header__option'>
+          <span className='header__optionLineOne'>
+            Hello Guest
+          </span>
+          <span className='header__optionLineTwo'>
+            Sign In
+          </span>
+        </div>
+
         <div className='header__option'>
           <span className='header__optionLineOne'>
             Returns
@@ -34,10 +44,6 @@ function Header() {
           <span className='header__optionLineTwo'>
             Prime
           </span>
-        </div>
-
-        <div className='header__option'>
-
         </div>
 
       </div>
