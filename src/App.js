34ec18b8d8c4b331e0211fea49a6f7854f25e02a
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-23 22:28:36 +0800

    add home, container, imageand css home__image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -8,8 +9,6 @@ function App() {
     <div className="App">
       <Header />
       <Home />
-      {/* Header */}
-      {/* Home */}
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+            <div className="home__container">
+                <img className="home__image"
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220.jpg" alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Home
