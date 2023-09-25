35038dbb5dbf5b148c36fabc235884ac2cd374f8
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-26 00:03:34 +0800

    add product id, title, price, image, rating

--- src/Home.js
+++ src/Home.js
@@ -11,19 +11,43 @@ function Home() {
 
                 <div className="home__row">
                     <Product
-                        title="The lean startup"
+                        id="12321341"
+                        title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
                         price={29.99}
                         image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
                         rating={5}
-
                     />
-                    <Product />
+                    <Product
+                        id="49538094"
+                        title="KenWood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 litre Glass Bowl"
+                        price={239.0}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/81rgIlDm6wL.__AC_SX300_SY300_QL70_ML2_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product
+                        id="4903850"
+                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                        price={199.99}
+                        rating={3}
+                        image="https://m.media-amazon.com/images/I/81rus0UFhsL._AC_SX679_.jpg"
+                    />
+                    <Product
+                        id="23445930"
+                        title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                        price={98.99}
+                        rating={5}
+                        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+                    />
+                    <Product
+                        id="3254354345"
+                        title="New Apple iPad Pro (12.9-inch, Wi-fi, 128GB) - Silver (4th Generation)"
+                        price={598.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Product.css";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
     return (
         <div className='product'>
             <div className='product__info'>
@@ -11,11 +11,15 @@ function Product({ title, image, price, rating }) {
                     <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>🌟</p>
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>⭐</p>
+                    ))}
                 </div>
             </div>
 
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+            <img src={image} alt="" />
             <button>Add to Basket</button>
         </div>
     )
