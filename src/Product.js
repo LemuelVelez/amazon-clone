5b16ad352ed87dbbae089ef5d6c95a558bfcfeb0
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-25 22:56:48 +0800

    add product css

--- src/Product.js
+++ src/Product.js
@@ -12,11 +12,11 @@ function Product() {
                 </p>
                 <div className="product__rating">
                     <p>🌟</p>
-                    <p>🌟</p>
-                    <p>🌟</p>
                 </div>
             </div>
 
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+            <button>Add to Basket</button>
         </div>
     )
 }
