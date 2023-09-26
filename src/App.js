2419a5cc97e15d4964f069051178168f0db1fd28
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-26 10:46:49 +0800

    add Router, Route, Routes and function Checkout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     // BEM
-    <div className="App">
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Checkout() {
+  return (
+    <>
       <Header />
-      <Home />
-    </div>
+      <h1>I AM CHECKOUT, SMASH THE LIKE BUTTON</h1>
+    </>
   );
 }
 
