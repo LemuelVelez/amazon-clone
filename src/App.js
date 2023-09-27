6c200c269e79235938d6303185c7dbd646c8f49a
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-27 17:09:08 +0800

    add checkout js and corresponding css

--- src/App.js
+++ src/App.js
@@ -2,28 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes, not Switch
+import Checkout from './Checkout';
 
 function App() {
   return (
     // BEM
     <Router>
       <div className="app">
-        <Routes>
+        <Header />
+        <Routes> {/* Use Routes instead of Switch */}
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
-  );
-}
-
-function Checkout() {
-  return (
-    <>
-      <Header />
-      <h1>I AM CHECKOUT, SMASH THE LIKE BUTTON</h1>
-    </>
   );
 }
 
