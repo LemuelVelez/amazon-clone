c57828f765203460d22eb2a2ba313c409eaa266a
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-28 11:35:27 +0800

    add basket?.length

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
 import { ShoppingBasket } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -56,7 +59,7 @@ function Header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasket />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
           </div>
         </Link>
       </div>
