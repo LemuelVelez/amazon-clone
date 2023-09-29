ee3ffc0c2a33b601af551bdd2a5a6f225299b632
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-29 19:38:25 +0800

    Add Login page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
   return (
+    // BEM
     <Router>
       <div className="app">
-        <Header />
         <Routes>
-          <Route path="login" element={<h1>Login page</h1>} /> {/* Wrap content in Route */}
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<><Login /></>} />
+          <Route path="/checkout" element={<><Header /><Checkout /></>} />
+          <Route path="/" element={<><Header /><Home /></>} />
         </Routes>
+
       </div>
     </Router>
   );
