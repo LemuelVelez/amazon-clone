b5c9ae88bf0011ab430f58e6993de1c6a9cca710
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-30 19:15:51 +0800

    update and fix errors

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from './firebase';
 
 function Login() {
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const signIn = e => {
-        e.preventDefault()
-
-        // some fancy firebase login.
-    }
-
-    const register = e => {
+    const signIn = async (e) => {
         e.preventDefault();
 
-        // do some fancy firebase register.
-    }
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            console.log('User signed in:', user);
+            navigate('/');
+        } catch (error) {
+            console.error('Error signing in:', error);
+            alert(error.message);
+        }
+    };
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            console.log('User registered:', user);
+            navigate('/');
+        } catch (error) {
+            console.error('Error registering user:', error);
+            alert(error.message);
+        }
+    };
 
     return (
         <div className='login'>
@@ -31,25 +49,24 @@ function Login() {
             <div className='login__container'>
                 <h1>Sign-in</h1>
 
-                <form>
+                <form onSubmit={signIn}>
                     <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
 
-                    <button type='submit' onClick={signIn}
-                        className='login__signInButton'>Sign In</button>
+                    <button type='submit' className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
-                    By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                    By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice, and our Interest-Based Ads Notice.
                 </p>
 
                 <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
-    )
+    );
 }
 
 export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,24 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+
+// Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDmLIv1C19LwO43TYymk8lTRZZYHmZzSqQ",
-    authDomain: "clone-664be.firebaseapp.com",
-    projectId: "clone-664be",
-    storageBucket: "clone-664be.appspot.com",
-    messagingSenderId: "58208125075",
-    appId: "1:58208125075:web:6616ad63007e417f27cd66",
-    measurementId: "G-6ZQQC4NLK9"
-  };
+  apiKey: "AIzaSyDmLIv1C19LwO43TYymk8lTRZZYHmZzSqQ",
+  authDomain: "clone-664be.firebaseapp.com",
+  projectId: "clone-664be",
+  storageBucket: "clone-664be.appspot.com",
+  messagingSenderId: "58208125075",
+  appId: "1:58208125075:web:6616ad63007e417f27cd66",
+  measurementId: "G-6ZQQC4NLK9"
+};
+
+// Initialize the Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get Firestore and Auth instances
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+// Export the instances
+export { db, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
