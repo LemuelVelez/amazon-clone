56a42c1979d9baac52f726e1944f52f16024af03
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-09-30 19:35:51 +0800

    add sign and sign out functionality

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import { ShoppingBasket } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
 
   return (
     <div className="header">
@@ -17,25 +24,18 @@ function Header() {
         />
       </Link>
 
-      <div
-        className="header__search">
-        <input
-          className="header__searchInput" type="text"
-        />
-        <SearchIcon
-          className="header__searchIcon" />
-        {/* Logo */}
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
-        <Link to='/login'>
-          <div className='header__option'>
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthentication} className='header__option'>
             <span className='header__optionLineOne'>
               Hello Guest
             </span>
-            <span className='header__optionLineTwo'>
-              Sign In
-            </span>
+            <span className='header__optionLineTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </Link>
 
@@ -64,7 +64,6 @@ function Header() {
           </div>
         </Link>
       </div>
-
     </div>
   );
 }
